0d3d2a6f9ba2b8eec6c0561660efc03172741735
Mary Jo Gray <mj81000@gmail.com>
2021-01-14 22:38:02 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+
+def turn_count(board)
+  turn = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turn+1
+  end
+  return turn
+end
+
+def current_player(board)
+  if turn_count%2 == 0
+    "X"
+  else
+    "O"
+  end
+end
